1463e9cf4766f9bc3404442b4fd2cd8b496f9880
vcashFan <vcashFan@email.com>
2016-12-23 22:21:29 +0000

    Add class for dialogs showing some kind of information.

---
+++ include/ShowInfoDialog.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * wxVcashGUI: a GUI for Vcash, a decentralized currency
+ *             for the internet (https://vcash.info).
+ *
+ * Copyright (c) The Vcash Developers
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ ******************************************************************************/
+
+#ifndef SHOWINFODIALOG_H
+#define SHOWINFODIALOG_H
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+#include <wx/dialog.h>
+#include <wx/sizer.h>
+#include <wx/window.h>
+#endif
+
+#include <functional>
+
+namespace wxGUI {
+    class ShowInfoDialog : public wxDialog {
+    public:
+        ShowInfoDialog(wxWindow &parent, const wxString &title, std::function<wxSizer *(void)> contents);
+    };
+}
+
+#endif // SHOWINFODIALOG_H
